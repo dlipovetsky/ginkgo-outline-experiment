1aec1ec58127bed817807b8200732ea90d59c570
Daniel Lipovetsky <dlipovetsky@d2iq.com>
2020-12-24 13:44:23 -0800

    Add outline tests

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module daniel.lipovetsky.me/ginkgo-outline
 
 go 1.13
 
-require golang.org/x/tools v0.0.0-20201223010750-3fa0e8f87c1a
+require (
+	github.com/google/go-cmp v0.4.0
+	golang.org/x/tools v0.0.0-20201223010750-3fa0e8f87c1a
+)
---
+++ outline/outline_test.go
@@ -0,0 +1,60 @@
+package outline
+
+import (
+	"encoding/json"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestFromASTFile(t *testing.T) {
+	tcs := []struct {
+		srcFile     string
+		outlineFile string
+	}{
+		{
+			srcFile:     "focused_test.go",
+			outlineFile: "focused_test_outline.json",
+		},
+		{
+			srcFile:     "pending_test.go",
+			outlineFile: "pending_test_outline.json",
+		},
+		{
+			srcFile:     "suite_test.go",
+			outlineFile: "suite_test_outline.json",
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.srcFile, func(t *testing.T) {
+			fset := token.NewFileSet()
+			astFile, err := parser.ParseFile(fset, filepath.Join("testdata", tc.srcFile), nil, 0)
+			if err != nil {
+				log.Fatalf("error parsing source: %s", err)
+			}
+
+			o, err := FromASTFile(fset, astFile)
+			if err != nil {
+				t.Fatalf("error creating outline %s", err)
+			}
+
+			got, err := json.MarshalIndent(o, "", "  ")
+			if err != nil {
+				log.Fatalf("error marshalling outline to json: %s", err)
+			}
+			want, err := ioutil.ReadFile(filepath.Join("testdata", tc.outlineFile))
+			if err != nil {
+				log.Fatalf("error reading outline fixture: %s", err)
+			}
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Fatalf("output mismatch (-want, +got):\n%s", diff)
+			}
+		})
+	}
+}
